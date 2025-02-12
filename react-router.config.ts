d67efb757e48db02a2064d5e63f943ff6dfca1ff
tnantaki <tinyhave@gmail.com>
2025-02-12 08:25:12 +0700

    config react router

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import defineConfig from "./vite.config"
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  basename: defineConfig.base,
+  ssr: false,
 } satisfies Config;
